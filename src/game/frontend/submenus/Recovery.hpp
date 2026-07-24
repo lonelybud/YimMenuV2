d23a6a604573ef02c5dec97ef72f8c306d8000d1
lonelybud <139460769+lonelybud@users.noreply.github.com>
2026-07-24 19:39:19 +0530

    fix delete pv

--- src/game/frontend/submenus/Recovery.hpp
+++ src/game/frontend/submenus/Recovery.hpp
@@ -111,43 +111,43 @@ namespace YimMenu::Submenus
 
 			components::checkbox(YimMenu::Features::_LSCCustomsBypass);
 
-			// components::ver_space();
-			// static bool delete_pv = false;
-			// static std::string pv_name;
-			// if (ImGui::Button("Delete Active Personal Vehicle"))
-			// 	FiberPool::Push([] {
-			// 		if (auto veh = PersonalVehicles::GetCurrentHandle(); veh.IsValid())
-			// 		{
-			// 			delete_pv = true;
-			// 			pv_name = PersonalVehicles::GetCurrent()->GetName();
-			// 		}
-			// 	});
-			// if (delete_pv)
-			// 	ImGui::OpenPopup("##delete_pv");
-			// if (ImGui::BeginPopupModal("##delete_pv", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove))
-			// {
-			// 	ImGui::Text("Are you sure you want to delete %s?", pv_name.c_str());
-			// 	ImGui::Spacing();
-			// 	if (ImGui::Button("Yes"))
-			// 	{
-			// 		FiberPool::Push([] {
-			// 			// https://www.unknowncheats.me/forum/grand-theft-auto-v/707419-lua-scripts-yimmenuv2-collection-thread-28.html#post4582934
-			// 			ScriptGlobal request(2733138);
-			// 			*request.At(472).As<int*>() = PersonalVehicles::GetCurrent()->GetId();
-			// 			*request.At(473).As<int*>() = 1;
-			// 			*request.At(474).As<int*>() = 0;
-			// 		});
-			// 		delete_pv = false;
-			// 		ImGui::CloseCurrentPopup();
-			// 	}
-			// 	ImGui::SameLine();
-			// 	if (ImGui::Button("No"))
-			// 	{
-			// 		delete_pv = false;
-			// 		ImGui::CloseCurrentPopup();
-			// 	}
-			// 	ImGui::EndPopup();
-			// }
+			components::ver_space();
+			static bool delete_pv = false;
+			static std::string pv_name;
+			if (ImGui::Button("Delete Active Personal Vehicle"))
+				FiberPool::Push([] {
+					if (auto veh = PersonalVehicles::GetCurrentHandle(); veh.IsValid())
+					{
+						delete_pv = true;
+						pv_name = PersonalVehicles::GetCurrent()->GetName();
+					}
+				});
+			if (delete_pv)
+				ImGui::OpenPopup("##delete_pv");
+			if (ImGui::BeginPopupModal("##delete_pv", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove))
+			{
+				ImGui::Text("Are you sure you want to delete %s?", pv_name.c_str());
+				ImGui::Spacing();
+				if (ImGui::Button("Yes"))
+				{
+					FiberPool::Push([] {
+						// https://www.unknowncheats.me/forum/grand-theft-auto-v/707419-lua-scripts-yimmenuv2-collection-thread-45.html#post4749525
+						ScriptGlobal request(2733326);
+						*request.At(472).As<int*>() = PersonalVehicles::GetCurrent()->GetId();
+						*request.At(473).As<int*>() = 1;
+						*request.At(474).As<int*>() = 0;
+					});
+					delete_pv = false;
+					ImGui::CloseCurrentPopup();
+				}
+				ImGui::SameLine();
+				if (ImGui::Button("No"))
+				{
+					delete_pv = false;
+					ImGui::CloseCurrentPopup();
+				}
+				ImGui::EndPopup();
+			}
 
 			components::ver_space();
 
